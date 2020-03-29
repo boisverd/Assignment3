1943041d96d3d5c47c02fac540986bd6bee1b916
boogy <boogy@LAPTOP-FINCL50A>
2020-03-29 15:44:37 -0400

    third commit

--- src/warmup3.java
+++ src/warmup3.java
@@ -2,14 +2,23 @@ public class warmup3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int coordinates[] = {9,1,9,2,9,3,9,4,9,5,9,6,9,7,9,8,9,9};
+		int coordinates[] = {1,9,2,9,3,9,4,9,5,9,6,9,7,9,8,9,9,9};
 		System.out.println("^"); //print top arrowhead
 		
 		for(int y=0;y<9;y++) {
+			System.out.print("|"); //vertical line
 			for(int i=0;i<18;i++)
-				System.out.print(coordinates[i]+" ");
+				if(i%2 == 0)
+					System.out.print("("+coordinates[i]+",");
+				else
+					if(y!=0) {
+						coordinates[i]=coordinates[i]-1;
+						System.out.print(coordinates[i]+")");}
+					else
+						System.out.print(coordinates[i]+")");
 			System.out.println("");
 		}
+		System.out.println("+--------------------------------------------->");
 	}
 
 }
